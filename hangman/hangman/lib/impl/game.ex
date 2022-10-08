2bb3b8dce41435f0d3f75d99466239f8dd4c9c58
Jay Geeseman <geesemanj@gmail.com>
2022-10-08 12:02:16 +0000

    Completed through "Pattern Matching Game State"

--- hangman/hangman/lib/impl/game.ex
+++ hangman/hangman/lib/impl/game.ex
@@ -28,4 +28,24 @@ defmodule Hangman.Impl.Game do
     }
   end
 
+  @spec make_move(t, String.t) :: { t, Type.tally }
+  def make_move(game = %{ game_state: state }, _guess)
+  when state in [:won, :lost] do
+    { game, tally(game) }
+  end
+
+  @spec make_move(t, String.t) :: { t, Type.tally }
+  def make_move(game, guess) do
+
+  end
+
+  @spec tally(t) :: Type.tally
+  defp tally(game) do
+    %{
+      turns_left: game.turns_left,
+      game_state: game.game_state,
+      letters: [],
+      used: game.used |> MapSet.to_list |> Enum.sort
+    }
+  end
 end
