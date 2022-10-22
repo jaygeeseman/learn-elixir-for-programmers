ac79a09ced55df87422fbe03ea96c87aef5c7315
Jay Geeseman <geesemanj@gmail.com>
2022-10-22 11:26:21 +0000

    Completed through "Then I'll Do It"

--- hangman/dictionary/lib/dictionary.ex
+++ hangman/dictionary/lib/dictionary.ex
@@ -1,11 +1,14 @@
 defmodule Dictionary do
+  alias Dictionary.Impl.WordList
 
-  @spec start() :: List.t
-  defdelegate start(), to: Dictionary.Impl.WordList
+  @opaque t :: WordList.t()
+
+  @spec start() :: t
+  defdelegate start(), to: WordList, as: :word_list
 
   @spec random_word() :: String.t
-  defdelegate random_word(), to: Dictionary.Impl.WordList
+  defdelegate random_word(), to: WordList
 
-  @spec random_word(List.t) :: String.t
-  defdelegate random_word(word_list), to: Dictionary.Impl.WordList
+  @spec random_word(t) :: String.t
+  defdelegate random_word(word_list), to: WordList
 end
--- hangman/dictionary/lib/impl/word_list.ex
+++ hangman/dictionary/lib/impl/word_list.ex
@@ -1,23 +1,20 @@
 defmodule Dictionary.Impl.WordList do
 
-  @spec start() :: List.t
-  def start() do
-    word_list()
-  end
+  @type t :: list(String.t())
 
-  @spec word_list(String.t) :: List.t
+  @spec word_list(String.t) :: t
   def word_list(filepath) do
     filepath
     |> File.read!()
     |> String.split(~r/\n/, trim: true)
   end
 
-  @spec word_list() :: List.t
+  @spec word_list() :: t
   def word_list() do
     word_list("assets/words.txt")
   end
 
-  @spec random_word(List.t) :: String.t
+  @spec random_word(t) :: String.t
   def random_word(word_list) do
     word_list
     |> Enum.random()
