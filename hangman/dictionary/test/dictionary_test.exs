4e494e94b28f93337a8725d8c57f95d90c4da96d
Jay Geeseman <geesemanj@gmail.com>
2022-10-22 12:56:30 +0000

    Completed through "Implement Dictionary Runtime"

--- hangman/dictionary/test/dictionary_test.exs
+++ hangman/dictionary/test/dictionary_test.exs
@@ -2,21 +2,9 @@ defmodule DictionaryTest do
   use ExUnit.Case
   doctest Dictionary
 
-  test "start/0 returns a list of words" do
-    word_list = Dictionary.start()
-    assert Enum.count(word_list) > 0
-    Enum.each(word_list, fn x -> assert String.length(x) > 0 end)
-  end
-
+  # This test will fail once in awhile
   test "random_word/0 returns random words" do
-    assert Dictionary.random_word() != Dictionary.random_word()
-  end
-
-  test "random_word/1 returns a random word from the given list" do
-    word_list = Dictionary.start()
-    assert Enum.member?(word_list, Dictionary.random_word(word_list))
-
-    word_list = ["notaword"]
-    assert Enum.member?(word_list, Dictionary.random_word(word_list))
+    { :ok, pid } = Dictionary.start_link()
+    assert Dictionary.random_word(pid) != Dictionary.random_word(pid)
   end
 end
