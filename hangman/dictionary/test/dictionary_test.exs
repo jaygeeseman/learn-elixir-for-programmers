97f402cc23ff5c9cc79f1f4f62f06a18a702dd92
Jay Geeseman <geesemanj@gmail.com>
2022-10-23 08:21:12 +0000

    Completed through "Run Dictionary as an Application"

--- hangman/dictionary/test/dictionary_test.exs
+++ hangman/dictionary/test/dictionary_test.exs
@@ -4,7 +4,6 @@ defmodule DictionaryTest do
 
   # This test will fail once in awhile
   test "random_word/0 returns random words" do
-    { :ok, pid } = Dictionary.start_link()
-    assert Dictionary.random_word(pid) != Dictionary.random_word(pid)
+    assert Dictionary.random_word() != Dictionary.random_word()
   end
 end
