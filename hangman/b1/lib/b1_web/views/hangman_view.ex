ea8d30c9a02df9d50b778abdffd973cb5a37c061
Jay Geeseman <geesemanj@gmail.com>
2022-11-19 07:39:31 +0000

    Completed through "Start to Tidy Up"

--- hangman/b1/lib/b1_web/views/hangman_view.ex
+++ hangman/b1/lib/b1_web/views/hangman_view.ex
@@ -1,3 +1,93 @@
 defmodule B1Web.HangmanView do
   use B1Web, :view
+
+  def figure_for(0) do
+    ~s{
+      +--+
+      |  |
+      o  |
+     /|\\ |
+     / \\ |
+         |
+    _____|_
+    }
+  end
+  def figure_for(1) do
+    ~s{
+      +--+
+      |  |
+      o  |
+     /|\\ |
+     /   |
+         |
+    _____|_
+    }
+  end
+  def figure_for(2) do
+    ~s{
+      +--+
+      |  |
+      o  |
+     /|\\ |
+         |
+         |
+    _____|_
+    }
+  end
+  def figure_for(3) do
+    ~s{
+      +--+
+      |  |
+      o  |
+     / \\ |
+         |
+         |
+    _____|_
+    }
+  end
+  def figure_for(4) do
+    ~s{
+      +--+
+      |  |
+      o  |
+     /   |
+         |
+         |
+    _____|_
+    }
+  end
+  def figure_for(5) do
+    ~s{
+      +--+
+      |  |
+      o  |
+         |
+         |
+         |
+    _____|_
+    }
+  end
+  def figure_for(6) do
+    ~s{
+      +--+
+      |  |
+         |
+         |
+         |
+         |
+    _____|_
+    }
+  end
+  def figure_for(7) do
+    ~s{
+      +--+
+         |
+         |
+         |
+         |
+         |
+    _____|_
+    }
+  end
+
 end
